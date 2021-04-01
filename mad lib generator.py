896d9900033f4f4792b794201159a2144a8f2222
Lokeshwari123 <64543226+Lokeshwari123@users.noreply.github.com>
2021-04-01 23:14:20 +0530

    Add files via upload

---
+++ mad lib generator.py
@@ -0,0 +1,11 @@
+#mad lib generator using python
+food=input("enter a type of food:")
+juli=input("enter a girl name:")
+adj1=input("enter an adjective:")
+bird=input("enter a noun:")
+verb1=input("enter a verb in past tense:")
+verb2=input("enetr a verb in the another past tense:")
+verb3=input("enetr a verb in third past tense:")
+noun1=input("enter a noun:")
+story= "It was " + food + " day at school, and " + juli+ " was super " + adj1 + " for lunch. But when she went outside to eat, a " + bird + " stole her " + food + "! " + juli + " chased the " + bird + " all over school. She " + verb1 + ", " + verb2 + ", and " + verb3 + " through the playground. Then she tripped on her " + noun1 + " and the " + bird + " escaped! Luckily, " + juli + "'s friends were willing to share their " + food + " with her."
+print(story)
